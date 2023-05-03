bde0d8991e7595eb913d2428c46761bd5e0b77c7
robi-yosef <129756055+robi-yosef@users.noreply.github.com>
2023-05-03 16:44:57 +0300

    Add files via upload

---
+++ INTRODUCTION_JS/InputOutput.js
@@ -0,0 +1,139 @@
+//תרגיל 1
+// 1.  צרו 2 משתנים המכילים מספר כלשהו..
+// אם 2 המספרים שווים , אני אדפיס לקונסול BOOM
+// אם המספרים לא שווים, אני אדפיס לקונסול TRY AGAIN.
+
+// let x = Number(prompt("pleas enter first number"));
+// let y= Number(prompt("pleas enter second number"));
+
+// console.log(x)
+// console.log(y)
+
+// if(x===y){
+//     alert("BOOM")
+// console.log("BOOM")
+// }else{
+// alert("TRY AGAIN")
+// console.log("TRY AGAIN")}
+
+
+// תרגיל 2
+// . צרו 2 משתנים והדפיסו לקונסול את הסכום של שניהם (חברו אותם)
+
+// let x = Number(prompt("pleas enter first number"));
+// let y= Number(prompt("pleas enter second number"));
+// let z=x+y
+
+// console.log (x)
+// console.log (y)
+// console.log (z)
+
+// תרגיל 3
+// צרו 2 משתנים והדפיסו לקונסול את המכפלה של שניהם.
+// let x = Number(prompt("pleas enter first number"));
+// let y= Number(prompt("pleas enter second number"));
+// let z=x*y
+
+// console.log (x)
+// console.log (y)
+// console.log (z)
+
+// תרגיל 4
+// צרו משתנה שמכיל מספר
+// שמייצג יום בשבוע ( 1 עבור יום ראשון , 2 עבור יום שני , 7 עבור שבת)
+// ואז הדפיסו הודעה בהתאם לאותו יום.
+// אם היום יום ראשון, הדפיסו good week
+// אם היום הוא : שני/שלישי/ רביעי : הדפיסו good day
+// אם היום הוא חמישי/שישי/שבת הדפיסו : happy weekend !
+
+// let x = prompt("pleas enter day number (1-7)");
+// console.log (x)
+
+// if(x==1){
+//     alert("GOOD WEEK")
+//     console.log("GOOD WEEK")
+// }else {''};
+
+
+// if(x==2|| x==3|| x==4){
+//     alert("GOOD DAY")
+//     console.log("GOOD DAY")
+// }else{''};
+
+// if(x==5|| x==6|| x==7){
+//     alert ("HAPPY WEEKEND!")
+//     console.log ("HAPPY WEEKEND!")
+// }else{''};
+
+// תרגיל 5
+// 5. כתוב משתנה שמכיל מספר שמייצג יום בחודש.( מ1 עד 31)
+// אם התאריך הוא 1 לחודש : אני אדפיס good month
+// אם התאריך הוא בין 2-10 לחודש אני אדפיס : start work
+// אם התאריך הוא 10 לחודש אני אדפיס : get salary
+// אם התאריך הוא בין 11-20 לחודש אני אדפיס : BE HAPPY
+// אם התאריך הוא בין 20-30 לחודש אני אדפיס: the end
+// אם התאריך הוא 31 לחודש אני אדפיס : BOOM BOOM
+
+
+// let x = prompt("pleas enter the day number of the month (1-31)");
+// console.log (x)
+
+// if(x==1){
+//     alert("good month")
+//     console.log("good month")
+// }else {''};
+
+
+// if(x==2|| x==3|| x==4|| x==5|| x==6|| x==7|| x==8|| x==9){
+//     alert("start work")
+//     console.log("start work")
+// }else{''};
+
+// if(x==10){
+//     alert ("get salary")
+//     console.log ("get salary")
+// }else{''};
+
+// if(x==11|| x==12|| x==13|| x==14|| x==15|| x==16|| x==17|| x==18|| x==19){
+//     alert ("BE HAPPY")
+//     console.log ("BE HAPPY")
+// }else{''};
+
+// if(x==20|| x==21|| x==22|| x==23|| x==24|| x==25|| x==26|| x==27|| x==28|| x==29|| x==30){
+//     alert ("the end")
+//     console.log ("the end")
+// }else{''};
+
+// if(x==31){
+//     alert ("BOOM BOOM")
+//     console.log ("BOOM BOOM")
+// }else{''};
+
+// 6. כתבו משתנה שמכיל שם של חג כ string
+// אם השם הוא purim – אני אדפיס : happy purim
+// אם השם הוא passover- אני אדפיס : clear home
+// אם השם הוא shavoout – אני אדפיס – eat milk
+// אם השם הוא suckot – אני אדפיס build sucka
+
+
+// let Holiday = prompt("What holiday is coming up?(Purim, Passover, Shavoout, Suckot)")
+// let x = "Purim"
+// let y = "passover"
+// let z = "shavoout"
+// let q = "suckot"
+
+// if ( Holiday = x){
+//     alert ("HAPPY PURIM :)")
+// }else {''};
+
+// if (Holiday = y) {
+//     alert ("CLEAR THE DRAWERS")
+// }else {''};
+
+// if ( Holiday = y){
+//     alert ("EAT MILK")
+// }else {''};
+
+// if ( Holiday = q){
+//     alert ("BUILD SUCKA")
+// }else {''};
